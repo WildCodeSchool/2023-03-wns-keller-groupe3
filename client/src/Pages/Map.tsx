eb853c4ca3d257797ba5e1cf28dc4572704f13f5
so-gau <sophie.gaubert10@gmail.com>
2023-07-05 15:00:09 +0200

    removes excess import

--- client/src/Pages/Map.tsx
+++ client/src/Pages/Map.tsx
@@ -1,17 +1,16 @@
 import "leaflet/dist/leaflet.css";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
 
 const lat = 50.6333;
 const long = 3.0667;
 
 export default function Map() {
   return (
-    <div id='map'>
+    <div id="map">
       <MapContainer center={[lat, long]} zoom={14} scrollWheelZoom={false}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         <Marker position={[50.6333, 3.0667]}>
           <Popup>Bar à Bière</Popup>
