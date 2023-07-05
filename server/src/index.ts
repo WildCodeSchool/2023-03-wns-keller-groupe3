1ac9ee60685b42dc6ecca305937e0515fca0f45d
so-gau <sophie.gaubert10@gmail.com>
2023-07-05 11:48:19 +0200

    resolve conflicts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,7 @@
-import { ApolloServer } from "apollo-server";
+import "reflect-metadata";
+import * as jwt from "jsonwebtoken";
 import dataSource from "./utils";
+import { ApolloServer } from "apollo-server";
 import { buildSchema } from "type-graphql";
 import { UserResolver } from "./resolvers/UserResolver";
 import { CityResolver } from "./resolvers/CityResolver";
@@ -9,9 +11,33 @@ const start = async (): Promise<void> => {
 
   const typeGraphQLgeneratedSchema = await buildSchema({
     resolvers: [UserResolver, CityResolver],
+    authChecker: ({ context }) => {
+      console.log("context from authchecker", context);
+      if (context.email !== undefined) {
+        return true;
+      } else {
+        return false;
+      }
+    },
   });
 
-  const server = new ApolloServer({ schema: typeGraphQLgeneratedSchema });
+  const server = new ApolloServer({
+    schema: typeGraphQLgeneratedSchema,
+    context: ({ req }) => {
+      console.log("req", req.headers.authorization);
+      if (
+        req.headers.authorization !== undefined &&
+        req.headers.authorization !== ""
+      ) {
+        const payload = jwt.verify(
+          req.headers.authorization.split("Bearer")[1],
+          "supersecretkey"
+        );
+        return payload;
+      }
+      return {};
+    },
+  });
 
   const { url } = await server.listen();
   console.log(`Server ready at ${url}`);
