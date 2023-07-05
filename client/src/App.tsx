9e8e815e453769cc071ae76e6650d9a04a3b22b3
Thomas <thomas.dicola@hotmail.fr>
2023-07-05 14:41:15 +0200

    init react-leaflet

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-import Home from './Pages/Home';
-import City from './Pages/City';
-
-import './App.css';
+import Home from "./Pages/Home";
+import City from "./Pages/City";
+import Map from "./Pages/Map";
+import "./App.css";
 
 function App() {
   return (
@@ -12,16 +11,20 @@ function App() {
         <nav>
           <ul>
             <li>
-              <Link to="/">Home</Link>
+              <Link to='/'>Home</Link>
             </li>
             <li>
-              <Link to="/city">City</Link>
+              <Link to='/city'>City</Link>
+            </li>
+            <li>
+              <Link to='/map'>Map</Link>
             </li>
           </ul>
         </nav>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/city" element={<City />} />
+          <Route path='/' element={<Home />} />
+          <Route path='/city' element={<City />} />
+          <Route path='/map' element={<Map />} />
         </Routes>
       </div>
     </Router>
