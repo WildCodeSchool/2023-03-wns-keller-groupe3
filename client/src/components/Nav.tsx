fe7f150c088ba2212f150874c13dfeb604980b74
Thomas <thomas.dicola@hotmail.fr>
2023-07-06 16:43:48 +0200

    add nav bar

---
+++ client/src/components/Nav.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Sidebar() {
+  return (
+    <ul className='z-10 fixed h-full menu bg-base-200  w-20 flex justify-center items-center'>
+      <li className='mb-5'>
+        <Link to='/'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            className='h-5 w-5'
+            fill='none'
+            viewBox='0 0 24 24'
+            stroke='currentColor'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeWidth='2'
+              d='M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6'
+            />
+          </svg>
+        </Link>
+      </li>
+      <li>
+        <Link to='/city'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            className='h-5 w-5'
+            fill='none'
+            viewBox='0 0 24 24'
+            stroke='currentColor'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeWidth='2'
+              d='M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z'
+            />
+          </svg>
+        </Link>
+      </li>
+    </ul>
+  );
+}
