e60e4677fd66e7bbaea2136e835596bdfd6e28af
LudovicLefieux <lefieux.ludovic@protonmail.com>
2023-07-06 17:07:10 +0200

    Stylize Cities page with fake data

--- client/src/Pages/City.tsx
+++ client/src/Pages/City.tsx
@@ -1,19 +1,44 @@
 import { Link } from "react-router-dom";
 import { mockData } from "../utils/mockData";
+import colmar from "../assets/images/colmar.jpg";
 
 function City() {
   return (
-    <>
-      <h1>City</h1>
+    <section className="container max-w-5xl md:mx-auto p-5 flex flex-col gap-4">
+      <h1 className="text-2xl font-bold ">Choisissez votre ville</h1>
+      <div className="border border-primary rounded-full flex gap-2 p-2">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+          />
+        </svg>
+        <input className="w-full" placeholder="Recherche" />
+      </div>
       {/* Liste de ville */}
-      <ul>
+      <ul className="container grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
         {mockData.map((c) => (
           <Link key={c.id} to={`/city/${c.id}`}>
-            <li>{c.name}</li>
+            <div className="card bg-base-100 shadow-xl">
+              <figure>
+                <img src={colmar} alt="City" />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">{c.name}</h2>
+              </div>
+            </div>
           </Link>
         ))}
       </ul>
-    </>
+    </section>
   );
 }
 
