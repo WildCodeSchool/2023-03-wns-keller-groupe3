72406369ec9110cb4356adb430e28718313789e5
Thomas <thomas.dicola@hotmail.fr>
2023-07-07 11:42:56 +0200

    delete state and refactor, rename Assets to assets

--- client/src/components/Map.tsx
+++ client/src/components/Map.tsx
@@ -1,6 +1,6 @@
 import "leaflet/dist/leaflet.css";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import bar from "../Assets/bar.png";
+import bar from "../assets/bar.png";
 
 interface POI {
   picture: string;
@@ -21,7 +21,7 @@ interface MapProps {
 
 export default function Map({ id, lat, long, poi }: MapProps) {
   return (
-    <div id="map">
+    <div id='map'>
       <MapContainer
         id={id}
         center={[lat, long]}
@@ -30,54 +30,54 @@ export default function Map({ id, lat, long, poi }: MapProps) {
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
         />
         {poi.map((p) => (
           <Marker position={[p.poiLat, p.poiLong]}>
             <Popup>
               <div>
-                <div className="leafletLine"></div>
-                <div className="w-full h-[190px]">
+                <div className='leafletLine'></div>
+                <div className='w-full h-[190px]'>
                   <img
-                    className="w-full h-full rounded-t-2xl"
+                    className='w-full h-full rounded-t-2xl'
                     src={bar}
-                    alt="test"
+                    alt='test'
                   />
                 </div>
-                <div className="px-[20px] pb-[30px]">
-                  <div className="py-[15px]">
-                    <h1 className="text-xl font-bold">{p.name}</h1>
+                <div className='px-[20px] pb-[30px]'>
+                  <div className='py-[15px]'>
+                    <h1 className='text-xl font-bold'>{p.name}</h1>
                   </div>
-                  <div className="flex border-y border-[#1B2F02]">
-                    <div className="flex-1 text-center py-2.5 hover:bg-[#1B2F021A] border-r border-[#1B2F02]">
-                      <a href="">Présentation</a>
+                  <div className='flex border-y border-[#1B2F02]'>
+                    <div className='flex-1 text-center py-2.5 hover:bg-[#1B2F021A] border-r border-[#1B2F02]'>
+                      <a href=''>Présentation</a>
                     </div>
-                    <div className="flex-1 text-center py-2.5 hover:bg-[#1B2F021A]">
-                      <a href="">Avis</a>
+                    <div className='flex-1 text-center py-2.5 hover:bg-[#1B2F021A]'>
+                      <a href=''>Avis</a>
                     </div>
                   </div>
-                  <div className="flex py-4 border-b border-[#1B2F02]">
-                    <div className="w-4/5">
-                      <p className="!m-0">{p.address}</p>
+                  <div className='flex py-4 border-b border-[#1B2F02]'>
+                    <div className='w-4/5'>
+                      <p className='!m-0'>{p.address}</p>
                     </div>
-                    <div className="w-1/5 flex justify-end items-end">
+                    <div className='w-1/5 flex justify-end items-end'>
                       <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
+                        xmlns='http://www.w3.org/2000/svg'
+                        fill='none'
+                        viewBox='0 0 24 24'
                         strokeWidth={1.5}
-                        stroke="currentColor"
-                        className="w-6 h-6"
+                        stroke='currentColor'
+                        className='w-6 h-6'
                       >
                         <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
+                          strokeLinecap='round'
+                          strokeLinejoin='round'
+                          d='M15 10.5a3 3 0 11-6 0 3 3 0 016 0z'
                         />
                         <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
+                          strokeLinecap='round'
+                          strokeLinejoin='round'
+                          d='M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z'
                         />
                       </svg>
                     </div>
