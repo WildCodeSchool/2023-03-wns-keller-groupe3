3bdc030fcd6d5cb7a11c210ab052ab8b0ae49d44
Thomas <thomas.dicola@hotmail.fr>
2023-07-07 09:15:34 +0200

    mobile view

--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -1,17 +1,106 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import logo from "../assets/logo.png";
 
 export default function Sidebar() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div
-      className={`z-10 fixed h-full menu bg-base-200 w-${
-        isOpen ? "48" : "20"
-      } flex justify-between rounded-r-xl shadow-[5px_0px_20px_0px_#00000024]`}
+      className={`z-10 fixed bottom-0 w-full h-14  md:fixed md:h-full md:menu bg-base-200 ${
+        isOpen ? "md:w-48" : "md:w-20"
+      } flex justify-between rounded-tl-xl rounded-tr-xl md:rounded-r-xl md:rounded-tl-none shadow-[5px_0px_20px_0px_#00000024]`}
     >
+      <img
+        alt='logo'
+        className='hidden md:block md:mask mask-circle w-14 mx-auto'
+        src={logo}
+      />
+      <ul className='flex items-center justify-around w-full md:flex-col md:items-start mx-auto'>
+        <li className='md:mb-5'>
+          <Link to='/'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              stroke-width='1.5'
+              stroke='currentColor'
+              className='w-6 h-6'
+            >
+              <path
+                stroke-linecap='round'
+                stroke-linejoin='round'
+                d='M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25'
+              />
+            </svg>
+            {isOpen ? <p className='ml-2'>Acceuil</p> : null}
+          </Link>
+        </li>
+        <li className='md:mb-5'>
+          <Link to='/city'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              stroke-width='1.5'
+              stroke='currentColor'
+              className='w-6 h-6'
+            >
+              <path
+                stroke-linecap='round'
+                stroke-linejoin='round'
+                d='M9 6.75V15m6-6v8.25m.503 3.498l4.875-2.437c.381-.19.622-.58.622-1.006V4.82c0-.836-.88-1.38-1.628-1.006l-3.869 1.934c-.317.159-.69.159-1.006 0L9.503 3.252a1.125 1.125 0 00-1.006 0L3.622 5.689C3.24 5.88 3 6.27 3 6.695V19.18c0 .836.88 1.38 1.628 1.006l3.869-1.934c.317-.159.69-.159 1.006 0l4.994 2.497c.317.158.69.158 1.006 0z'
+              />
+            </svg>
+            {isOpen ? <p className='ml-2'>Villes</p> : null}
+          </Link>
+        </li>
+        <li className='md:mb-5'>
+          <Link to='/user'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              stroke-width='1.5'
+              stroke='currentColor'
+              className='w-6 h-6'
+            >
+              <path
+                stroke-linecap='round'
+                stroke-linejoin='round'
+                d='M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z'
+              />
+            </svg>
+            {isOpen ? <p className='ml-2'>Profil</p> : null}
+          </Link>
+        </li>
+        <li>
+          <Link to='/'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              stroke-width='1.5'
+              stroke='currentColor'
+              className='w-6 h-6'
+            >
+              <path
+                stroke-linecap='round'
+                stroke-linejoin='round'
+                d='M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75'
+              />
+            </svg>
+            {isOpen ? (
+              <p className='lowercase font-normal'>Se déconnecter</p>
+            ) : null}
+          </Link>
+        </li>
+      </ul>
       <div className='mx-auto'>
-        <button className='btn btn-sm' onClick={() => setIsOpen(!isOpen)}>
+        <button
+          className='hidden md:block btn btn-sm'
+          onClick={() => setIsOpen(!isOpen)}
+        >
           {!isOpen ? (
             <svg
               xmlns='http://www.w3.org/2000/svg'
@@ -45,86 +134,6 @@ export default function Sidebar() {
           )}
         </button>
       </div>
-      <ul className='flex flex-col mx-auto'>
-        <li className='mb-5'>
-          <Link to='/'>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              fill='none'
-              viewBox='0 0 24 24'
-              stroke-width='1.5'
-              stroke='currentColor'
-              className='w-6 h-6'
-            >
-              <path
-                stroke-linecap='round'
-                stroke-linejoin='round'
-                d='M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25'
-              />
-            </svg>
-            {isOpen ? <p className='ml-2'>Acceuil</p> : null}
-          </Link>
-        </li>
-        <li className='mb-5'>
-          <Link to='/city'>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              fill='none'
-              viewBox='0 0 24 24'
-              stroke-width='1.5'
-              stroke='currentColor'
-              className='w-6 h-6'
-            >
-              <path
-                stroke-linecap='round'
-                stroke-linejoin='round'
-                d='M9 6.75V15m6-6v8.25m.503 3.498l4.875-2.437c.381-.19.622-.58.622-1.006V4.82c0-.836-.88-1.38-1.628-1.006l-3.869 1.934c-.317.159-.69.159-1.006 0L9.503 3.252a1.125 1.125 0 00-1.006 0L3.622 5.689C3.24 5.88 3 6.27 3 6.695V19.18c0 .836.88 1.38 1.628 1.006l3.869-1.934c.317-.159.69-.159 1.006 0l4.994 2.497c.317.158.69.158 1.006 0z'
-              />
-            </svg>
-            {isOpen ? <p className='ml-2'>Villes</p> : null}
-          </Link>
-        </li>
-        <li>
-          <Link to='/user'>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              fill='none'
-              viewBox='0 0 24 24'
-              stroke-width='1.5'
-              stroke='currentColor'
-              className='w-6 h-6'
-            >
-              <path
-                stroke-linecap='round'
-                stroke-linejoin='round'
-                d='M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z'
-              />
-            </svg>
-            {isOpen ? <p className='ml-2'>Profil</p> : null}
-          </Link>
-        </li>
-      </ul>
-      <ul className='flex justify-center mb-5'>
-        <button className='btn'>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            fill='none'
-            viewBox='0 0 24 24'
-            stroke-width='1.5'
-            stroke='currentColor'
-            className='w-6 h-6'
-          >
-            <path
-              stroke-linecap='round'
-              stroke-linejoin='round'
-              d='M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75'
-            />
-          </svg>
-          {isOpen ? (
-            <p className='lowercase font-normal'>Se déconnecter</p>
-          ) : null}
-        </button>
-      </ul>
     </div>
   );
 }
