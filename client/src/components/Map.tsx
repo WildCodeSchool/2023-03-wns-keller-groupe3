afb084ee6dc6eaff3079f5125d44bf8f2370d473
so-gau <sophie.gaubert10@gmail.com>
2023-07-07 10:21:36 +0200

    delete div

--- client/src/components/Map.tsx
+++ client/src/components/Map.tsx
@@ -56,35 +56,33 @@ export default function Map({ id, lat, long, poi }: MapProps) {
                       <a href="">Avis</a>
                     </div>
                   </div>
-                  <div>
-                    <div className="flex py-4 border-b border-[#1B2F02]">
-                      <div className="w-4/5">
-                        <p className="!m-0">{p.address}</p>
-                      </div>
-                      <div className="w-1/5 flex justify-end items-end">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth={1.5}
-                          stroke="currentColor"
-                          className="w-6 h-6"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
-                          />
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
-                          />
-                        </svg>
-                      </div>
+                  <div className="flex py-4 border-b border-[#1B2F02]">
+                    <div className="w-4/5">
+                      <p className="!m-0">{p.address}</p>
+                    </div>
+                    <div className="w-1/5 flex justify-end items-end">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-6"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
+                        />
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
+                        />
+                      </svg>
                     </div>
                   </div>
-                  <span> Catégorie : {p.category}</span>
+
                   <br />
                   <span>{p.description}</span>
                 </div>
