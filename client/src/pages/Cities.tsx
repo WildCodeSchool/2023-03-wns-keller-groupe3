ec7b856b96a0492431b8a7e1f9a471b092658674
Thomas <thomas.dicola@hotmail.fr>
2023-07-10 16:18:31 +0200

    add city front

--- client/src/pages/Cities.tsx
+++ client/src/pages/Cities.tsx
@@ -20,7 +20,7 @@ function Cities() {
 
   return (
     <section className='container max-w-5xl md:mx-auto p-5 flex flex-col gap-6'>
-      <h1 className='text-2xl font-bold '>Choisissez votre ville</h1>
+      <h1 className='text-xl font-bold mt-5'>Choisissez votre ville</h1>
       <div className='border border-grey-900 rounded-full flex gap-2 p-2 bg-white'>
         <svg
           xmlns='http://www.w3.org/2000/svg'
@@ -38,6 +38,26 @@ function Cities() {
         </svg>
         <input className='w-full' placeholder='Recherche' />
       </div>
+      <label
+        htmlFor='my_modal_6'
+        className='md:fixed md:top-5 right-5 btn btn-primary'
+      >
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          fill='none'
+          viewBox='0 0 24 24'
+          stroke-width='1.5'
+          stroke='currentColor'
+          className='w-6 h-6'
+        >
+          <path
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            d='M12 4.5v15m7.5-7.5h-15'
+          />
+        </svg>
+        ajouter
+      </label>
       {/* Liste de villes */}
       <ul className='container grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6'>
         {data.getAllCities.map((city: City) => (
@@ -46,6 +66,60 @@ function Cities() {
           </Link>
         ))}
       </ul>
+      <input type='checkbox' id='my_modal_6' className='modal-toggle' />
+      <div className='modal'>
+        <div className='modal-box'>
+          <h3 className='font-bold text-lg mb-4'>Ajouter une ville</h3>
+          <hr></hr>
+          <form
+            className='py-6 flex flex-col'
+            action='
+          '
+          >
+            <label className='text-sm mb-2' htmlFor='name'>
+              Nom :
+            </label>
+            <input
+              id='name'
+              type='text'
+              placeholder='Paris, Rome, Rio ...'
+              className='input input-bordered w-full max-w-xs mb-4'
+            />
+            <label className='text-sm mb-2' htmlFor='name'>
+              Image :
+            </label>
+            <input
+              type='text'
+              placeholder='url : https://...'
+              className='input input-bordered w-full max-w-xs mb-4'
+            />
+            <label className='text-sm mb-2' htmlFor='name'>
+              Latitude :
+            </label>
+            <input
+              type='text'
+              placeholder='43.2345'
+              className='input input-bordered w-full max-w-xs mb-4'
+            />
+            <label className='text-sm mb-2' htmlFor='name'>
+              Longitude :
+            </label>
+            <input
+              type='text'
+              placeholder='2.5456'
+              className='input input-bordered w-full max-w-xs'
+            />
+          </form>
+          <div className='modal-action flex justify-between'>
+            <label htmlFor='my_modal_6' className='btn'>
+              Annuler
+            </label>
+            <label htmlFor='my_modal_6' className='btn btn-primary'>
+              Valider
+            </label>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
