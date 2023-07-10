327bb4c9c8aa559670726a5d09e05cb62457ac23
VictorGugg <victor.guggenbuhl@gmail.com>
2023-07-10 10:34:08 +0200

    Improvement on Home Page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -10,11 +10,11 @@ function Home() {
         <div className="hero-overlay bg-opacity-60"></div>
         <div className="hero-content text-center text-neutral-content">
           <div className="max-w-2-xl">
-            <h1 className="mb-10 text-9xl font-bold outline-black">City Guide</h1>
+            <h1 className="mb-20 text-9xl font-bold outline-black">City Guide</h1>
             <p className="mb-2 text-2xl font-medium text-left ml-20">Parcourir une ville comme un local.</p>
             <p className="mb-2 text-2xl font-medium">Aller droit au but.</p>
-            <p className="mb-5 text-2xl font-medium text-right mr-20">Découvrir des perles rares.</p>
-            <Link to='/city'><button className="btn btn-primary">Explorez maintenant !</button></Link>
+            <p className="mb-10 text-2xl font-medium text-right mr-20">Découvrir des perles rares.</p>
+            <Link to='/city'><button className="btn btn-lg btn-block max-w-md btn-primary">Explorez maintenant !</button></Link>
           </div>
         </div>
       </div>
