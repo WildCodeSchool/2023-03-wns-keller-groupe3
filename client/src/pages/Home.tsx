1f66baab6f48a75941d1b3667af2b9b6bbe58db2
Thomas <thomas.dicola@hotmail.fr>
2023-07-10 14:16:25 +0200

    fix size for responsive

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -10,16 +10,16 @@ function Home() {
       <div className='hero-overlay bg-opacity-60'></div>
       <div className='hero-content text-center text-neutral-content'>
         <div className='max-w-2-xl'>
-          <h1 className='mb-20 text-9xl font-bold outline-black'>City Guide</h1>
-          <p className='mb-2 text-2xl font-medium text-left ml-20'>
+          <h1 className='mb-20 text-5xl font-bold'>City Guide</h1>
+          <p className='mb-2 text-xl font-medium'>
             Parcourir une ville comme un local.
           </p>
-          <p className='mb-2 text-2xl font-medium'>Aller droit au but.</p>
-          <p className='mb-10 text-2xl font-medium text-right mr-20'>
+          <p className='mb-2 text-xl font-medium'>Aller droit au but.</p>
+          <p className='mb-10 text-xl font-medium'>
             Découvrir des perles rares.
           </p>
           <Link to='/cities'>
-            <button className='btn btn-lg btn-block max-w-md btn-primary'>
+            <button className='btn btn-md btn-block max-w-md btn-primary'>
               Explorez maintenant !
             </button>
           </Link>
