96e6f3238637e500eb75b9d0f38381ff3748390d
so-gau <sophie.gaubert10@gmail.com>
2023-08-30 14:06:53 +0200

    margin top

--- client/src/pages/User.tsx
+++ client/src/pages/User.tsx
@@ -15,10 +15,10 @@ function UserPage() {
   return (
     <div
       className={`container relative flex flex-col items-center h-screen w-full px-9 md:pt-10 md:pl-16 md:pr-0 2xl:pl-0 xl:pt-0 2xl:pt-16 mx-auto
-       ${isRegisterMode ? "mb-[250px] md:mb-0" : ""}`}
+       ${isRegisterMode ? "mb-[250px] md:pb-8 md:mb-0" : ""}`}
     >
       <form
-        className="mt-8 max-w-sm flex flex-col w-full relative z-10"
+        className="mt-8 lg:mt-0 max-w-sm flex flex-col w-full relative z-10"
         onSubmit={async (e) => {
           e.preventDefault();
         }}
