7888627883c9d77550b2bb78b00f767ff5f394dc
VictorGugg <victor.guggenbuhl@gmail.com>
2023-09-01 11:14:28 +0200

    WIP

---
+++ client/src/components/Map.test.tsx
@@ -0,0 +1,11 @@
+// Check react testing library
+
+import Map from './Map'
+
+describe('Map', () => {
+    describe('double click on map', () => {
+        it('opens a POI modal', () => {
+            const mockDoubleClick = jest.fn();
+        })
+    })
+})
