3ed827bc1c0f258b9cbcb3a59e2f15c6b7b2aa50
Thomas <thomas.dicola@hotmail.fr>
2023-10-06 10:58:42 +0200

    fix import

--- client/src/pages/Cities.tsx
+++ client/src/pages/Cities.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { CityCard } from "../components/CityCard";
 import backgroundCity from "../assets/cityBackground.png";
