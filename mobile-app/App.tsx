fa8704e9be4bfb48e801e2d2a274578e8f755f2d
LudovicLefieux <lefieux.ludovic@protonmail.com>
2023-11-07 14:41:41 +0100

    Import assets

--- mobile-app/App.tsx
+++ mobile-app/App.tsx
@@ -1,11 +1,27 @@
+import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeScreen from "./src/pages/Home";
+import CitiesScreen from "./src/pages/Cities";
+import UserScreen from "./src/pages/User";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <Text>Coucou Cityguide !</Text>
       <StatusBar style="auto" />
+
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Accueil" component={HomeScreen} />
+          <Tab.Screen name="Villes" component={CitiesScreen} />
+          <Tab.Screen name="Profil" component={UserScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
     </View>
   );
 }
--- mobile-app/src/pages/Home.tsx
+++ mobile-app/src/pages/Home.tsx
@@ -1,26 +1,28 @@
 import backgroundImage from "../assets/europeMap.png";
 import { Link } from "react-router-dom";
+import { Text, View, Image, Button } from "react-native";
 
 function Home() {
   return (
-    <div
-      className='hero min-h-screen bg-base-content'
-      style={{ backgroundImage: `url(${backgroundImage})` }}
-    >
-      <div className='hero-overlay '></div>
-      <div className='hero-content text-center text-neutral-content'>
-        <div className='max-w-2-xl text-base-content'>
-          <h1 className='mb-5 text-5xl font-bold'>City Guide</h1>
-          <p className='mb-5'>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Image
+        style={{ flex: 1, width: "100%", resizeMode: "cover" }}
+        source={{ backgroundImage }}
+      />
+      <View className="hero-overlay "></View>
+      <View className="hero-content text-center text-neutral-content">
+        <View className="max-w-2-xl text-base-content">
+          <Text className="mb-5 text-5xl font-bold">City Guide</Text>
+          <Text className="mb-5">
             Parcourir une ville comme un local. Aller droit au but. <br />
             Découvrir des perles rares.
-          </p>
-          <Link to='/cities'>
-            <button className='btn btn-primary'>Explorez maintenant !</button>
+          </Text>
+          <Link to="/cities">
+            <Button className="btn btn-primary">Explorez maintenant !</Button>
           </Link>
-        </div>
-      </div>
-    </div>
+        </View>
+      </View>
+    </View>
   );
 }
 
