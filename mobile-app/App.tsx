bb194c9e6e074a11ef684dfeef5f0b79d461a54e
LudovicLefieux <lefieux.ludovic@protonmail.com>
2023-11-08 11:31:04 +0100

    Adding navigation icons

--- mobile-app/App.tsx
+++ mobile-app/App.tsx
@@ -1,17 +1,39 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "./src/pages/Home";
+import CitiesScreen from "./src/pages/Cities";
+import UserScreen from "./src/pages/User";
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName: keyof typeof Ionicons.glyphMap = "home";
+            if (route.name === "Accueil") {
+              iconName = focused ? "home" : "home-outline";
+            } else if (route.name === "Villes") {
+              iconName = focused ? "map" : "map-outline";
+            } else if (route.name === "Profil") {
+              iconName = focused ? "person-circle" : "person-circle-outline";
+            }
+
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: "blue",
+          tabBarInactiveTintColor: "gray",
+        })}
+      >
+        <Tab.Screen name="Accueil" component={HomeScreen} />
+        <Tab.Screen name="Villes" component={CitiesScreen} />
+        <Tab.Screen name="Profil" component={UserScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
