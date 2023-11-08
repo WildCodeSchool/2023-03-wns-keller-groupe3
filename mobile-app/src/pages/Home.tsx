e5c80c3ad972d374e45ba3cbff79ede8368429b6
LudovicLefieux <lefieux.ludovic@protonmail.com>
2023-11-08 09:58:31 +0100

    Clear Home

--- mobile-app/src/pages/Home.tsx
+++ mobile-app/src/pages/Home.tsx
@@ -1,29 +1,9 @@
-import backgroundImage from "../assets/europeMap.png";
-import { Link } from "react-router-dom";
-import { Text, View, Image, Button } from "react-native";
+// import backgroundImage from "../../assets/europeMap.png";
+import React from "react";
+import { Text } from "react-native";
 
 function Home() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Image
-        style={{ flex: 1, width: "100%", resizeMode: "cover" }}
-        source={{ backgroundImage }}
-      />
-      <View className="hero-overlay "></View>
-      <View className="hero-content text-center text-neutral-content">
-        <View className="max-w-2-xl text-base-content">
-          <Text className="mb-5 text-5xl font-bold">City Guide</Text>
-          <Text className="mb-5">
-            Parcourir une ville comme un local. Aller droit au but. <br />
-            Découvrir des perles rares.
-          </Text>
-          <Link to="/cities">
-            <Button className="btn btn-primary">Explorez maintenant !</Button>
-          </Link>
-        </View>
-      </View>
-    </View>
-  );
+  return <Text>Home Screen</Text>;
 }
 
 export default Home;
