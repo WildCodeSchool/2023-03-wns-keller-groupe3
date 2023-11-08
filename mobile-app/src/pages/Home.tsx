6acd29ae098dabc582d1c772f521bb0b4306eb94
LudovicLefieux <lefieux.ludovic@protonmail.com>
2023-11-08 14:56:55 +0100

    Thomas changes

--- mobile-app/src/pages/Home.tsx
+++ mobile-app/src/pages/Home.tsx
@@ -1,9 +1,27 @@
-// import backgroundImage from "../../assets/europeMap.png";
 import React from "react";
-import { Text } from "react-native";
+import { Text, View, Pressable } from "react-native";
+import { NavigationContainer, RouteProp } from "@react-navigation/native";
 
-function Home() {
-  return <Text className="text-red-600">Home Screen</Text>;
+// Créez un type pour la stack de navigation
+
+function Home({ navigation }) {
+  return (
+    <View className="hero-content text-center text-neutral-content">
+      <View className="max-w-2-xl text-base-content">
+        <Text className="mb-5 text-5xl font-bold">City Guide</Text>
+        <Text className="mb-5">
+          Parcourir une ville comme un local. Aller droit au but. Découvrir des
+          perles rares.
+        </Text>
+        <Pressable
+          className="btn"
+          onPress={() => navigation.navigate("Villes")}
+        >
+          <Text>Explorez maintenant !</Text>
+        </Pressable>
+      </View>
+    </View>
+  );
 }
 
 export default Home;
