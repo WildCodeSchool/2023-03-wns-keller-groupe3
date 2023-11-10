29ca89276e7645caa60b76603f75cf0375d41a25
so-gau <sophie.gaubert10@gmail.com>
2023-11-10 11:31:36 +0100

    feat responsive for desktop

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -27,53 +27,58 @@ function Home() {
 
     <div className="md:ml-24">
       <div
-        className="homeback flex relative justify-center h-[400px] bg-cover bg-no-repeat bg-center items-center"
+        className="homeback flex relative justify-center xl:h-[30vh] bg-cover bg-no-repeat bg-center items-center"
         style={{ backgroundImage: `url(${backgroundImage})` }}
       >
-        <h1 className="text-white text-5xl relative z-10 font-bold">
-          Bienvenue dans notre Guide City !
+        <h1 className="text-[#1f1b23] text-5xl relative z-10 font-bold">
+          Bienvenue sur City Guide !
         </h1>
       </div>
-      <div className="bg-[#fefefe] p-9">
-        <div className="flex justify-center items-end">
+      <div
+        className="2xl:p-9 xl:h-[36vh]"
+        style={{
+          background: "linear-gradient(180deg, #EFEFEF 0%, #C3C3C3 100%)",
+        }}
+      >
+        <div className="flex justify-around h-full items-center">
           <div className="card-home">
             <div>
-              <h3 className="mb-4 text-[#1f1b23] text-center">
-                Laissez-vous guider par notre site : trouvez facilement les
-                points d'intérêt, les attractions, et les expériences uniques de
-                chaque destination
+              <h3 className="text-[#1f1b23] text-center">
+                <b>Laissez-vous guider</b> par notre site : trouvez facilement
+                les points d'intérêt, les attractions, et les expériences
+                uniques de chaque destination
               </h3>
             </div>
-            <div className="rounded-img">
-              <img src={icon1} alt="" />
+            <div className="mt-3.5">
+              <img src={icon1} alt="icon mountain" />
             </div>
           </div>
           <div className="card-home">
             <div>
-              <h3 className="mb-4 text-[#1f1b23] text-center">
-                Explorez les villes où vous souhaitez vous rendre : plongez dans
-                la culture locale, goûtez à la cuisine authentique, et explorez
-                des quartiers pittoresques.
+              <h3 className="text-[#1f1b23] text-center">
+                <b>Explorez les villes</b> où vous souhaitez vous rendre :
+                plongez dans la culture locale, goûtez à la cuisine authentique,
+                et explorez des quartiers pittoresques.
               </h3>
             </div>
-            <div className="rounded-img">
-              <img src={icon2} alt="" />
+            <div className="mt-3.5">
+              <img src={icon3} alt="icon explore" />
             </div>
           </div>
           <div className="card-home">
             <div>
-              <h3 className="mb-4 text-[#1f1b23] text-center">
-                Faites des rencontres mémorables : rencontrez les habitants,
-                partagez des expériences, et créez des liens.{" "}
+              <h3 className="text-[#1f1b23] text-center">
+                <b>Faites des rencontres</b> mémorables : rencontrez les
+                habitants, partagez des expériences, et créez des liens.{" "}
               </h3>
             </div>
-            <div className="rounded-img">
-              <img src={icon3} alt="" />
+            <div className="mt-3.5">
+              <img src={icon2} alt="icon meet" />
             </div>
           </div>
         </div>
       </div>
-      <div>
+      <div className="xl:h-[33vh] flex justify-center items-center">
         <Link to="/cities">
           <button className="btn btn-primary">Explorez maintenant !</button>
         </Link>
