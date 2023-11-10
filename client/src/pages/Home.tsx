194eb31fe1b51785af3f48c8ed8be7ce651fb8f1
so-gau <sophie.gaubert10@gmail.com>
2023-11-10 13:21:41 +0100

    added test

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -4,19 +4,20 @@ import { Link } from "react-router-dom";
 function Home() {
   return (
     <div
-      className='hero min-h-screen bg-base-content'
+      className="hero min-h-screen bg-base-content"
       style={{ backgroundImage: `url(${backgroundImage})` }}
     >
-      <div className='hero-overlay '></div>
-      <div className='hero-content text-center text-neutral-content'>
-        <div className='max-w-2-xl text-base-content'>
-          <h1 className='mb-5 text-5xl font-bold'>City Guide</h1>
-          <p className='mb-5'>
+      test
+      <div className="hero-overlay "></div>
+      <div className="hero-content text-center text-neutral-content">
+        <div className="max-w-2-xl text-base-content">
+          <h1 className="mb-5 text-5xl font-bold">City Guide</h1>
+          <p className="mb-5">
             Parcourir une ville comme un local. Aller droit au but. <br />
             Découvrir des perles rares.
           </p>
-          <Link to='/cities'>
-            <button className='btn btn-primary'>Explorez maintenant !</button>
+          <Link to="/cities">
+            <button className="btn btn-primary">Explorez maintenant !</button>
           </Link>
         </div>
       </div>
