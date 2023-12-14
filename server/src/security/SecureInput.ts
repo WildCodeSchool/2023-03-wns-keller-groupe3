7e54708aea47b6ede8cf25230796755ae73d1993
LudovicLefieux <lefieux.ludovic@protonmail.com>
2023-12-14 15:27:15 +0100

    Refactoring test regex

--- server/src/security/SecureInput.ts
+++ server/src/security/SecureInput.ts
@@ -4,30 +4,27 @@ export function SecureEmail(input: string): string {
   const emailRegex =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-  if (input.trim() !== "" && emailRegex.test(input)) {
-    return input.trim();
-  } else {
-    console.error("Bad email format");
-    throw new ApolloError("Bad email format");
-  }
+  return testRegex(input, emailRegex, "email");
 }
 
 export function SecurePassword(input: string): string {
   const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).\S{8,}$/;
 
-  if (input.trim() !== "" && passwordRegex.test(input)) {
-    return input.trim();
-  } else {
-    console.error("Bad password format");
-    throw new ApolloError("Bad password format");
-  }
+  return testRegex(input, passwordRegex, "password");
 }
 
 export default function SecureInput(input: string): string {
-  const regex = /[&$<>{}/\\"*[\]]/gm;
+  const regex = /^[^&$<>{}\\"*[\]]+$/gm;
 
-  if (input.trim() !== "" && !regex.test(input)) {
+  return testRegex(input, regex, "text");
+}
+
+function testRegex(input: string, regex: RegExp, type: string): string {
+  if (input.trim() !== "" && regex.test(input)) {
     return input.trim();
+  } else if (type !== "text") {
+    console.error(`Bad ${type} format`);
+    throw new ApolloError(`Bad ${type} format`);
   } else {
     console.error(`The input "${input}" contains forbidden characters`);
     throw new ApolloError(`The input "${input}" contains forbidden characters`);
